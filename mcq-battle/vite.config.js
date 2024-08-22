bde76bfa1b4d8cc8432a89810cff304cb3119092
TanushreePrasad <107496822+TanushreePrasad@users.noreply.github.com>
2024-08-22 12:10:39 +0530

    add files

---
+++ mcq-battle/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  publicDir: 'public',
+  // No need to specify publicDir unless it's not in 'public'
+})
